9f2b7ebbebd31df115c09974dc543a47d2beff41
Gabriel Alvaro <gabriel.batista.1801@gmail.com>
2022-04-02 20:25:13 -0300

    Update q2_script.R

--- code/q2_script.R
+++ code/q2_script.R
@@ -77,8 +77,8 @@ q2_base = q2_data_merged %>%
 
 q2_base[is.na(q2_base)] = 0
 
-q2_acionamento_outliers = c((quantile(q2_data_merged$QTD_ACIONAMENTO_6M)[2])-1.5*IQR(q2_data_merged$QTD_ACIONAMENTO_6M),
-                            (quantile(q2_data_merged$QTD_ACIONAMENTO_6M)[4]+1.5*IQR(q2_data_merged$QTD_ACIONAMENTO_6M)))
+q2_acionamento_outliers = c((quantile(q2_data_merged$QTD_ACIONAMENTO)[2])-1.5*IQR(q2_data_merged$QTD_ACIONAMENTO),
+                            (quantile(q2_data_merged$QTD_ACIONAMENTO)[4]+1.5*IQR(q2_data_merged$QTD_ACIONAMENTO)))
 
 
 # exportando o dataframe completo como arquivo txt
